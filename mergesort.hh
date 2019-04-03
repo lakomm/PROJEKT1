fc86b83b118a8bcc2643a377ff1c60a404bd0060
Michał Łakomski <mikilak@onet.eu>
2019-04-03 22:48:48 +0200

    mergesort class

--- mergesort.hh
+++ mergesort.hh
@@ -5,7 +5,6 @@
 #include<windows.h>
 using namespace std;
 
-
 class Mergesort{
 public:
     template<typename T>
@@ -14,7 +13,6 @@ public:
 
      T *dodTablica;
      dodTablica=new T[rozmiar];
-
      mergeSort(tablicaDoPosortowania, 0, rozmiar-1, dodTablica);
 
     stop=clock();
